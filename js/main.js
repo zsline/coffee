3071cd089f6e8a0da8d1e8ae57924b75dce3b245
zsline <sergei.dereka@gmail.com>
2022-01-13 17:48:20 +0200

    dded contacts

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+  $('.best__slider').slick({
+    dots: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="../images/prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next slick-btn"><img src="../images/next.svg" alt=""></button>',
+    });
+});
