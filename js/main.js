e0305534024f4c2137876f849cc09beddfc0b3d4
zsline <sergei.dereka@gmail.com>
2022-01-14 13:15:08 +0200

    js puth

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(function(){
     dots: false,
     slidesToShow: 3,
     slidesToScroll: 1,
-    prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="../images/prev.svg" alt=""></button>',
-    nextArrow: '<button type="button" class="slick-next slick-btn"><img src="../images/next.svg" alt=""></button>',
+    prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="./images/prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next slick-btn"><img src="./images/next.svg" alt=""></button>',
     });
 });
